9b6f8ff01505b33b1d560b92af7d607671a8996b
joseangel9898 <damcb164e28@gmail.com>
2015-11-18 12:03:11 -0600

    version acciones
    version 2

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,23 @@
 // JavaScript Document
+
+function consulta()
+{
+	datos="";
+	$.ajax({
+		    type: "POST", 
+			url: "192.168.1.30/procesar.php", 
+			datos: datos 
+	       }).done(function(mensaje){
+		alert (mensaje);
+	});
+}
+
+
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
-}); 
-});
-
+		$("#btnconsulta").on("tap", function(){
+			alert ("preguntando..");
+	        consulta();
+        });
+ });//device ready
+});//document ready
